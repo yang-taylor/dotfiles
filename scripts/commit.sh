3f412d54284c31329bc802de8f1fb07d472e7f1f
taylor y <64106592+taylory-xyz@users.noreply.github.com>
2021-04-08 20:58:23 -0400

    [04/08] fix: updated packages, fixed commit script, edited url for create script

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,15 +1,14 @@
 echo -e "\nwhat type of commit is this?"
-echo -e "\t 1: docs"
-echo -e "\t 2: feature/addition"
-echo -e "\t 3: fix"
-echo -e "\t 4: refactor"
-echo -e "\t 5: format"
-echo -e "\t 6: override"
+echo -e "\t1: docs"
+echo -e "\t2: addition"
+echo -e "\t3: fix"
+echo -e "\t4: refactor"
+echo -e "\t5: override"
 
 read option
 
 create_message() {
-	echo -e "[$(date +%m/%d)] $type\n $(git status)" >| ~/.gitmessage.txt
+	echo -e "[$(date +%m/%d)] $type\n" >| ~/.gitmessage.txt
 	git commit
 	git push
 }
@@ -18,7 +17,7 @@ if [ $option -eq "1" ]; then
 	type="docs: "
 	create_message
 elif [ $option -eq "2" ]; then
-	type="feature: "
+	type="addition: "
 	create_message
 elif [ $option -eq "3" ]; then
 	type="fix: "
@@ -27,9 +26,7 @@ elif [ $option -eq "4" ]; then
 	type="refactor: "
 	create_message
 elif [ $option -eq "5" ]; then
-	type="format: "
-	create_message
-elif [ $option -eq "6" ]; then
+	echo -e "[$(date +%m/%d)] " >| ~/.gitmessage.txt
 	git commit
 	git push
 else 
--- scripts/create.sh
+++ scripts/create.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
-echo -e "\n what type of file do you want to create?"
-echo -e "\t 1: html"
-echo -e "\t 2: tex (chem)"
-echo -e "\t 3: tex (self-notes)"
-echo -e "\t 4: javafx build.gradle"
+echo -e "\nwhat type of file do you want to create?"
+echo -e "\t1: html"
+echo -e "\t2: tex (self-notes)"
+echo -e "\t3: javafx build.gradle"
 
 read filetype
 
-root="https://www.tayloryang.xyz"
+root="https://gists.tayleyi.com"
 
-htmlURL="${root}/gists/template.html"
+htmlURL="${root}/template.html"
 
-texURL="${root}/gists/notes.tex"
+texURL="${root}/personal-notes.tex"
 
 if [ $filetype -eq "1" ]; then
 	curl $htmlURL > $1
 fi
 
 if [ $filetype -eq "2" ]; then
-	mkdir temp_folder
-	git clone https://gist.github.com/6d7ad097c163e733d984adbf1d3b8c41.git temp_folder
-	cd temp_folder
-	mv *.tex ..
-	cd ..
-	rm temp_folder -rf
+	curl $texURL > $1
 fi
 
 if [ $filetype -eq "3" ]; then
-	curl $texURL > $1
+	curl "${root}/fx-build.gradle" > $1
 fi
